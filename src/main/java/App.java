f0a15280b02e82c85a3c73d876e6eb591776fceb
MikBV <bmv00791@gmail.com>
2024-10-29 13:23:46 +0800

    Exersize complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCatBean = (Cat) applicationContext.getBean("cat");
+        Cat secondCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == secondBean);
+        System.out.println(firstCatBean == secondCatBean);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public void sayHello () {
+        System.out.println("Myau-myau");
+    }
+}
